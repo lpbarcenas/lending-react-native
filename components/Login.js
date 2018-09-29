a055f502007503919b26755ab76722daec1c9b73
leodel <lpbarce@gmail.com>
2018-09-29 17:44:26 +0800

    initial lending app commit

---
+++ components/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet, Image, TextInput } from 'react-native';
+
+const styles = StyleSheet.create({
+    image: {
+        width: 100, 
+        height: 100,
+        alignSelf: 'center'
+    },
+    input: {
+        height: 50,
+        width: 200,
+        marginTop: 30,
+        padding: 4,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'green'
+    }
+});
+
+export default class Login extends Component {
+
+    render() {
+        return (
+            <View>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/images/lending-logo.png')}
+                />
+                <TextInput style={styles.input} 
+                    placeholder="Username"
+                />
+                <TextInput style={styles.input} 
+                    placeholder="Password"
+                    secureTextEntry={true}
+                />
+            </View>
+        )
+    }
+}
